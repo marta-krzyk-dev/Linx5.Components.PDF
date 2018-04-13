01c33ed0f0a41a770bf2a26494b15c283bdbdd30
evl <esteban.villegas.lastra@gmail.com>
2018-04-13 12:01:16 -0500

    story: LINXFIVE-2458 Add PDF.Split refactor. reviewer: JC.

---
+++ src/Pdf/Split/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace Twenty57.Linx.Components.Pdf.Split
+{
+	public class PropertyNames
+	{
+		public const string LoopResults = "Loop results";
+		public const string ExecutionPathName = "PageFiles";
+
+		public const string OutputNumberOfPages = "NumberOfPages";
+		public const string OutputPageFiles = "PageFiles";
+	}
+}
